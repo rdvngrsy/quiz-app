86eed702b29b6b3fe9bc7791f4bd658edf16c253
rdvngrsy <ridvangursoy@hotmail.com>
2024-03-09 23:08:40 +0300

    Renk değişikliği.

--- src/components/QuestionsCard/QuestionsCard.tsx
+++ src/components/QuestionsCard/QuestionsCard.tsx
@@ -2,8 +2,12 @@ import React, { useEffect, useState } from "react";
 import { QuestionsDataInfo } from "../../models/QuestionsData/questionsDataResponse";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "../../store/store";
-import { setCorrectAnswers, setQuestions, setUserAnswer } from "../../store/slices/answersSlice";
-import "./QuestionsCard.css"
+import {
+  setCorrectAnswers,
+  setQuestions,
+  setUserAnswer,
+} from "../../store/slices/answersSlice";
+import "./QuestionsCard.css";
 
 type Props = {
   questionsData: QuestionsDataInfo[];
@@ -20,60 +24,63 @@ const QuestionsCard = (props: Props) => {
   const answersState = useSelector((state: any) => state.answers);
   const dispatch = useDispatch<AppDispatch>();
 
-
   const approvedChoice = (e: any) => {
     //if (timer > 20) return;
     console.log(e.currentTarget.value);
-    const checkAnswer = e.currentTarget.value == props.questionsData[props.count]?.correct_answer;
+    const checkAnswer =
+      e.currentTarget.value == props.questionsData[props.count]?.correct_answer;
     if (checkAnswer) {
-      props.setScore(props.score+10)
+      props.setScore(props.score + 10);
     }
-    dispatch(setUserAnswer(e.currentTarget.value))
-    
-    props.setCount(props.count+1)
-  
-    if(props.count == 9) props.setNext(true)
-    setTimer(30)
-  };
+    dispatch(setUserAnswer(e.currentTarget.value));
 
+    props.setCount(props.count + 1);
+
+    if (props.count == 9) props.setNext(true);
+    setTimer(30);
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
       if (timer > 0) {
-        setTimer(timer-1)
+        setTimer(timer - 1);
       }
-      if(timer == 0 &&  props.count < 10){
-        props.setCount(props.count+1)
-        dispatch(setUserAnswer(""))
-        setTimer(30)
-      }else if(props.count >= 10){
-        props.setNext(true)
+      if (timer == 0 && props.count < 10) {
+        props.setCount(props.count + 1);
+        dispatch(setUserAnswer(""));
+        setTimer(30);
+      } else if (props.count >= 10) {
+        props.setNext(true);
       }
-    },1000)
-  
+    }, 1000);
 
     return () => {
-      clearInterval(interval)
-    }
-  }, [timer])
-  
+      clearInterval(interval);
+    };
+  }, [timer]);
+
   useEffect(() => {
     if (props.questionsData[props.count]) {
       dispatch(setQuestions(props.questionsData[props.count].body));
-      dispatch(setCorrectAnswers(props.questionsData[props.count].correct_answer))
+      dispatch(
+        setCorrectAnswers(props.questionsData[props.count].correct_answer)
+      );
     }
     console.log(props.count);
-    
+
     console.log(answersState);
   }, [props.count, props.questionsData]);
-
-  
-  
 
   return (
     <div>
       <div className="bg-white max-w-[1000px] w-full flex flex-col items-center p-8 rounded-3xl space-y-4 relative border-4 border-black shadow-[5px_5px_0px_0px_rgba(0,0,0)]">
-        <div className={`absolute mt-[-115px] bg-white w-40 h-40 font-bold text-[45px] flex justify-center items-center rounded-full border-4 border-black shadow-[5px_5px_0px_0px_rgba(0,0,0)] ${timer<10 ? "bg-red-600 text-white heartbeat" : "bg-white text-black"}`}>
+        <div
+          className={`absolute mt-[-115px] bg-white w-40 h-40 font-bold text-[45px] flex justify-center items-center rounded-full border-4 border-black shadow-[5px_5px_0px_0px_rgba(0,0,0)] ${
+            timer < 10
+              ? "bg-red-500 text-black heartbeat"
+              : "bg-white text-black"
+          }`}
+        >
           {timer}
         </div>
         <div className="font-normal text-[30px] pb-5 pt-9 ">
@@ -82,7 +89,9 @@ const QuestionsCard = (props: Props) => {
         {props.questionsData[props.count]?.answers?.map((answer, index) => (
           <button
             className={`w-full h-16 rounded-2xl bg-blue-700 text-white hover:bg-blue-300 hover:text-black font-bold text-[25px] tracking-wider shadow-[5px_5px_0px_0px_rgba(0,0,0)]
-            ${timer > 20 ? 'opacity-50 cursor-not-allowed' : ''} transition ease-in duration-200`}
+            ${
+              timer > 20 ? "opacity-50 cursor-not-allowed" : ""
+            } transition ease-in duration-200`}
             onClick={approvedChoice}
             key={index}
             value={answer}
